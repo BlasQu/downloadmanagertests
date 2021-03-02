67309c7122e5f8350c3d832f35c0a118217a2932
BlasQu <damianhs138@gmail.com>
2021-03-02 12:55:26 +0100

    Download dummy file with service and download manager + broadcast receiver

--- app/src/main/java/com/example/downloadfileswithservice/MainActivity.kt
+++ app/src/main/java/com/example/downloadfileswithservice/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.downloadfileswithservice
 
+import android.app.DownloadManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    var downloadid: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val new = object: BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+                if (id == downloadid) Log.d("DOWNLOAD", "DONE")
+            }
+        }
+
+        val uri = Uri.parse("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf")
+
+        val request = DownloadManager.Request(uri).setDescription("DummyFile").setTitle("Dummy").setAllowedOverMetered(true)
+
+        val location = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        downloadid = location.enqueue(request)
+
+        registerReceiver(new, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
     }
 }
